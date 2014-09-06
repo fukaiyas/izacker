cbde2d860406707c148f493baea19ae8a52e7404
fukaiyas <yasuyuki.fukai@gmail.com>
2014-09-06 18:30:21 +0900

    暫定版のアルゴリズム実装

--- src/main/scala/controller/RootController.scala
+++ src/main/scala/controller/RootController.scala
@@ -1,6 +1,10 @@
 package controller
 
-import model.Izakaya
+import java.util.Date
+
+import model.{ History, Izakaya }
+import org.joda.time.{ DateTime, LocalDate }
+import scalikejdbc._
 
 class RootController extends ApplicationController {
 
@@ -14,6 +18,22 @@ class RootController extends ApplicationController {
 
   private def chooseIzakaya(): Option[Izakaya] = {
     scala.util.Random.shuffle(Izakaya.findAll()).headOption
+
+    val last: Long = History.findAllWithLimitOffset(
+      limit = 1,
+      offset = 0,
+      orderings = Seq(History.defaultAlias.date.desc)
+    ).headOption.getOrElse(History(-1, -1, None, LocalDate.now, DateTime.now, DateTime.now)).izakayaId
+
+    val recentPeriod = LocalDate.fromDateFields(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 24 * 30 * 2))
+    val weight: Map[Long, Int] = History.findAllBy(where = sqls"date >= $recentPeriod")
+      .groupBy(_.izakayaId)
+      .mapValues(_.size)
+    logger.debug(weight)
+
+    Izakaya.findAllBy(where = sqls"id <> $last")
+      .sortBy(izakaya => izakaya.priority * 100 / weight.getOrElse(izakaya.id, 1))
+      .headOption
   }
 
 }
